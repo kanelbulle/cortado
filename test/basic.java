92897b3fffca2632c72145fa93b642d4d649d4e7
Samuel Wejeus <samuel.wejeus@gmail.com>
2012-03-06 17:30:12 +0100

    Added overloads to stmt/var for main method to

--- test/basic.java
+++ test/basic.java
@@ -1,16 +1,38 @@
 class Superklasse {
 	public static void main(String[] args) {
-		int myVariable;		
-		int secondVar;
-		int thirdVar;
-		secondVar = 1;
 	}
 }
 
 class Halvklasse {
 
-	public int fnurf(int n) {
+	public int fnurf() {
 		return 1;
 	}
 }
 
+/*
+class IfAnd
+{
+    public static void main(String[] a){
+	System.out.println(new Test().go());
+    }
+}
+
+class Test {
+    int a;
+
+    public int go() {
+	a = 17;
+	if(a < 13 && this.change()) {
+	}
+	else {
+	}
+	return a;
+    }
+
+    public boolean change() {
+	a = 4711;
+	return true;
+    }
+}
+*/
