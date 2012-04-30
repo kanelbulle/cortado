78f8aa3215dd316e21565c0371023310e845afc8
Emil Arfvidsson <emil@arfvidsson.com>
2012-04-30 16:13:40 +0200

    Add register lists in Hardware

--- src/se/cortado/frame/x86_64/Hardware.java
+++ src/se/cortado/frame/x86_64/Hardware.java
@@ -4,41 +4,44 @@ import se.cortado.ir.temp.Temp;
 
 @SuppressWarnings("unused")
 public class Hardware {
-	public static final int wordSize = 8;
+	public static final int		wordSize	= 8;
 
-	private static final Temp rax = new Temp();
-	private static final Temp rbc = new Temp();
-	private static final Temp rcx = new Temp();
-	private static final Temp rdx = new Temp();
+	private static final Temp	rax			= new Temp();
+	private static final Temp	rbx			= new Temp();
+	private static final Temp	rcx			= new Temp();
+	private static final Temp	rdx			= new Temp();
 
-	private static final Temp rsp = new Temp();
-	private static final Temp rbp = new Temp();
-	private static final Temp rsi = new Temp();
-	private static final Temp rdi = new Temp();
+	private static final Temp	rsp			= new Temp();
+	private static final Temp	rbp			= new Temp();
+	private static final Temp	rsi			= new Temp();
+	private static final Temp	rdi			= new Temp();
 
-	private static final Temp r8 = new Temp();
-	private static final Temp r9 = new Temp();
-	private static final Temp r10 = new Temp();
-	private static final Temp r11 = new Temp();
+	private static final Temp	r8			= new Temp();
+	private static final Temp	r9			= new Temp();
+	private static final Temp	r10			= new Temp();
+	private static final Temp	r11			= new Temp();
 
-	private static final Temp r12 = new Temp();
-	private static final Temp r13 = new Temp();
-	private static final Temp r14 = new Temp();
-	private static final Temp r15 = new Temp();
+	private static final Temp	r12			= new Temp();
+	private static final Temp	r13			= new Temp();
+	private static final Temp	r14			= new Temp();
+	private static final Temp	r15			= new Temp();
 
-	private static final Temp[] argRegs = { rdi, rsi, rdx, rcx, r8, r9 };
+	// TODO appels says at p. 199 that 'the four lists of registers must not
+	// overlap', but these for lists overlap in the x86 specification (some regs
+	// are both an arg reg and a special reg). what does he mean?
+	private static final Temp[]	specialRegs	= { rsp, rbp, rax, rdx };
 
-	private static final Temp[] retRegs = { rax, rdx };
+	private static final Temp[]	argRegs		= { rdi, rsi, rdx, rcx, r8, r9 };
 
-	public static final Temp SP = rsp;
-	public static final Temp FP = rbp;
-	public static final Temp RV = retRegs[0];
+	private static final Temp[]	calleeSaves	= {rbx, rsp, rbp, r12, r13, r14, r15};
+
+	private static final Temp[]	callerSaves	= {rax, rcx, rdx, rsi, rdi, r8, r9, r10, r11};
+
+	public static final Temp	SP			= rsp;
+	public static final Temp	FP			= rbp;
+	public static final Temp	RV			= rax;
 
 	public static Temp getArgReg(int index) {
 		return argRegs[index];
-	}
-
-	public static Temp getRetReg(int index) {
-		return retRegs[index];
 	}
 }
