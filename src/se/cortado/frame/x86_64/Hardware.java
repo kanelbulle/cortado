50013446cd16c5d62fbb133d0da2d4dccc341d86
Emil Arfvidsson <emil@arfvidsson.com>
2012-05-01 18:17:45 +0200

    Add reglists

--- src/se/cortado/frame/x86_64/Hardware.java
+++ src/se/cortado/frame/x86_64/Hardware.java
@@ -3,7 +3,6 @@ package se.cortado.frame.x86_64;
 import se.cortado.ir.temp.Temp;
 import se.cortado.ir.temp.TempList;
 
-@SuppressWarnings("unused")
 public class Hardware {
 	public static final int		wordSize	= 8;
 
@@ -30,22 +29,36 @@ public class Hardware {
 	// TODO appels says at p. 199 that 'the four lists of registers must not
 	// overlap', but these for lists overlap in the x86 specification (some regs
 	// are both an arg reg and a special reg). what does he mean?
-	private static final Temp[]	specialRegs	= { rsp, rbp, rax, rdx };
+	public static final Temp[]	specialRegs	= { rsp, rbp, rax, rdx };
 
-	private static final Temp[]	argRegs		= { rdi, rsi, rdx, rcx, r8, r9 };
+	public static final Temp[]	argRegs		= { rdi, rsi, rdx, rcx, r8, r9 };
 
-	private static final Temp[]	calleeSaves	= {rbx, rsp, rbp, r12, r13, r14, r15};
+	public static final Temp[]	calleeSaves	= { rbx, rsp, rbp, r12, r13, r14, r15 };
 
-	private static final Temp[]	callerSaves	= {rax, rcx, rdx, rsi, rdi, r8, r9, r10, r11};
+	public static final Temp[]	callerSaves	= { rax, rcx, rdx, rsi, rdi, r8, r9, r10, r11 };
 
 	public static final Temp	SP			= rsp;
 	public static final Temp	FP			= rbp;
 	public static final Temp	RV			= rax;
 
+	public static final TempList	calldefs;
+	
+	static {
+		TempList tl = null;
+		for (Temp t : callerSaves) {
+			if (tl == null) {
+				tl = new TempList(t, null);
+			} else {
+				tl = new TempList(t, tl);
+			}
+		}
+		calldefs = tl;
+	}
+
 	public static Temp getArgReg(int index) {
 		return argRegs[index];
 	}
-	
+
 	public static TempList calleeSavedList() {
 		TempList tl = new TempList(calleeSaves[0], null);
 		for (int i = 1; i < calleeSaves.length; i++) {
