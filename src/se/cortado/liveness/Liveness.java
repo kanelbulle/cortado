2c454704dcb968a9063c428c10626996e1e90c9b
Emil Arfvidsson <emil@arfvidsson.com>
2012-05-08 20:12:16 +0200

    Add Liveness class

---
+++ src/se/cortado/liveness/Liveness.java
@@ -0,0 +1,29 @@
+package se.cortado.liveness;
+
+import se.cortado.ir.temp.Temp;
+
+public class Liveness extends InterferenceGraph {
+
+	public Liveness(FlowGraph flow) {
+
+	}
+
+	@Override
+	public Node tnode(Temp temp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Temp gtemp(Node node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MoveList moves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
