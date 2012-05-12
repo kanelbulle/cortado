2a808cdb78119992dff06a0cc316f31055321d6d
Emil Arfvidsson <emil@arfvidsson.com>
2012-05-12 21:39:29 +0200

    Moar debugging

--- test/Simple.java
+++ test/Simple.java
@@ -1,19 +1,22 @@
 class Superklasse {
 	public static void main(String[] args) {
+		int i1;
+		int i2;
 		int resultat;
-		LillKlasse lk;
+		// LillKlasse lk;
 		
-		lk = new LillKlasse();
+		i1 = 3;
+		i2 = 120;
 		
-		resultat = lk.addera(7, 5);
+		resultat = i1 + i2;
 		
 		System.out.println(resultat);
 	}
 }
 
-class LillKlasse {
-	
-	public int addera(int a, int b) {
-		return a + b;
-	}
-}
+// class LillKlasse {
+// 	
+// 	public int addera(int a, int b) {
+// 		return a + b;
+// 	}
+// }
