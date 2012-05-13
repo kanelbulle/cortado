2fcf0d3caee067db231de32f0d9809b9581fcd6c
Emil Arfvidsson <emil@arfvidsson.com>
2012-05-14 00:33:19 +0200

    Add some jasmin output

--- src/se/cortado/visitors/JasminVisitor.java
+++ src/se/cortado/visitors/JasminVisitor.java
@@ -1,6 +1,9 @@
 package se.cortado.visitors;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import se.cortado.syntaxtree.And;
 import se.cortado.syntaxtree.ArrayAssign;
@@ -51,282 +54,330 @@ import se.cortado.syntaxtree.While;
 
 public class JasminVisitor implements Visitor {
 
-	File mFile;
+	BufferedWriter	mWriter;
+	
+	SymbolTable mSymbolTable;
+	ClassScope mClassScope;
+	MethodScope mMethodScope;
+	
+	public JasminVisitor(SymbolTable symbolTable) {
+		this.mSymbolTable = symbolTable;
+	}
+
+	public void write(String message) {
+		try {
+			mWriter.write(message + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void writei(String message) {
+		write("    " + message);
+	}
 
 	@Override
 	public void visit(And node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ArrayAssign node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ArrayLength node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ArrayLookup node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Assign node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Block node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(BooleanType node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Call node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ClassDecl node) {
-		// TODO Auto-generated method stub
-		
+		try {
+			// create file writer
+			mWriter = new BufferedWriter(new FileWriter(node.i.s + ".j"));
+
+			write(".class public " + node.i.s);
+			write(".super java/lang/Object");
+
+			write(".method public <init>()V");
+			writei("aload_0");
+			writei("invokenonvirtual java/lang/Object/<init>()V");
+			writei("return");
+			write(".end method");
+
+			mWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void visit(ClassDeclExtends node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ClassDeclList node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ClassDeclSimple node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Exp node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(ExpList node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(False node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Formal node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(FormalList node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Identifier node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(IdentifierExp node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(IdentifierType node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(If node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(IntArrayType node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(IntegerLiteral node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(IntegerType node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(LessThan node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(MainClass node) {
-		// TODO Auto-generated method stub
+		mClassScope = mSymbolTable.get(node.i.s);
 		
+		node.md.accept(this);
 	}
 
 	@Override
 	public void visit(MethodDecl node) {
 		// TODO Auto-generated method stub
+
+		mMethodScope = mClassScope.getMethodMatching(node);
 		
+		if (mClassScope.getClassDecl() instanceof MainClass) {
+			write(".method public static main([Ljava/lang/String;)V");
+		} else {
+			//write(".method public " + node.identifier.s +)
+		}
+		
+		
+		
+		node.exp.accept(this);
+		
+		write(".end method");
 	}
 
 	@Override
 	public void visit(MethodDeclList node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Minus node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(NewArray node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(NewObject node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Not node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Plus node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Print node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Program node) {
-		// TODO Auto-generated method stub
-		
+		node.mainClass.accept(this);
+		node.classDeclList.accept(this);
 	}
 
 	@Override
 	public void visit(Statement node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(StatementList node) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void visit(StringArrayType node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(This node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Times node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(True node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(Type node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(VarDecl node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(VarDeclList node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(VoidType node) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void visit(While node) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
